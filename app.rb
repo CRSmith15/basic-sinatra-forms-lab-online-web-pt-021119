64c4a8af1afc0825615f699a1934685b6751e186
Chance Smith <chancesmith97@hotmail.com>
2019-11-25 22:01:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,16 @@ class App < Sinatra::Base
   end
   
   post '/team' do 
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["pg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
     
+    erb :team 
+  end
   
 
   
